314d93ae19e86de1114b3cc4c80c8b38fdac85f1
Rebecca Wardle <mattydwardle@gmail.com>
2020-12-08 19:00:44 +0000

    Some bug fixing, refactoring in search of bug. pep8-ed craftmodule, added type to constructor

--- modules/craftmodule.py
+++ modules/craftmodule.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.5
 """Module class to hold data on individual modules that form part of the morsecraft structure"""
-from numpy import array, round
+from numpy import round
 
 __author__ = "Rebecca Wardle"
 __copyright__ = "Copyright 2020 Rebecca Wardle"
@@ -10,14 +10,21 @@ __version__ = "0.5"
 
 
 class Module:
-    """A module class that contains:
-    position, rotation, connections, type, dimensions,
-    id used within spacecraft"""
-    def __init__(self, mod_id, dimensions=(0.1, 0.1, 0.1), position=(0, 0, 0)):
+    """
+    A module class used in morsecraft
+    """
+    def __init__(self, mod_id, type, dimensions=(0.1, 0.1, 0.1), position=(0, 0, 0)):
+        """
+        Constructor
+        :param mod_id: string, unique identifier of the module
+        :param type: string, module type
+        :param dimensions: (optional) integer tuple, dimensions of mod (in m)
+        :param position: (optional) integer tuple, cartesian coords of mod
+        """
         self.cons = [None] * len(dimensions) * 2
         self.rotation = [1] + [0] * 3
         self.pos = round(position, 3)
-        self.type = None
+        self.type = type
         self.id = mod_id
         self.dims = dimensions
 
